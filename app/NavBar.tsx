0ecab936f0c711c011b00d5706aac4ee679c6d3f
Muhammad Hasbi <onlyhasbi@gmail.com>
2023-12-06 22:38:10 +0800

    implement authentication login and logout

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { Container, Flex } from '@radix-ui/themes';
-import classNames from 'classnames';
 import Link from 'next/link';
+import classNames from 'classnames';
+import { Box, Container, Flex } from '@radix-ui/themes';
+import { useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import { AiFillBug } from 'react-icons/ai';
 
@@ -13,6 +14,7 @@ const navbars = [
 
 function NavBar() {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
 
   return (
     <nav className="flex mb-5 h-14 items-center border-b">
@@ -23,19 +25,27 @@ function NavBar() {
           </Link>
           <ul className="flex gap-3">
             {navbars?.map((nav) => (
-              <Link
-                key={nav.label}
-                className={classNames({
-                  'text-zinc-900': nav.href === currentPath,
-                  'text-zinc-500': currentPath !== nav.href,
-                  'hover:text-zinc-800 transition-colors': true,
-                })}
-                href={nav.href}
-              >
-                {nav.label}
-              </Link>
+              <li key={nav.label}>
+                <Link
+                  className={classNames({
+                    'text-zinc-900': nav.href === currentPath,
+                    'text-zinc-500': currentPath !== nav.href,
+                    'hover:text-zinc-800 transition-colors': true,
+                  })}
+                  href={nav.href}
+                >
+                  {nav.label}
+                </Link>
+              </li>
             ))}
           </ul>
+          <Box>
+            {status === 'authenticated' ? (
+              <Link href="/api/auth/signout">Logout</Link>
+            ) : (
+              <Link href="/api/auth/signin">Login</Link>
+            )}
+          </Box>
         </Flex>
       </Container>
     </nav>
