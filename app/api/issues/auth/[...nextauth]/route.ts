83fe351d8d7f2468c0b0f79ea5883ab0eb8c18b2
Muhammad Hasbi <onlyhasbi@gmail.com>
2023-12-06 18:15:15 +0800

    configure google provider

--- app/api/issues/auth/[...nextauth]/route.ts
+++ app/api/issues/auth/[...nextauth]/route.ts
@@ -4,12 +4,10 @@ import GoogleProvider from 'next-auth/providers/google';
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GoogleProvider([
-      {
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      },
-    ]),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
     // ...add more providers here
   ],
 };
