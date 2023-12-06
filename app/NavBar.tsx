3dc289dfa7e7166c6d65faf3860a29745a43d7c6
Muhammad Hasbi <onlyhasbi@gmail.com>
2023-12-06 17:30:22 +0800

    set up next auth

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Container, Flex } from '@radix-ui/themes';
 import classNames from 'classnames';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -14,25 +15,29 @@ function NavBar() {
   const currentPath = usePathname();
 
   return (
-    <nav className="flex gap-6 px-5 mb-5 h-14 items-center border-b">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex gap-3">
-        {navbars?.map((nav) => (
-          <Link
-            key={nav.label}
-            className={classNames({
-              'text-zinc-900': nav.href === currentPath,
-              'text-zinc-500': currentPath !== nav.href,
-              'hover:text-zinc-800 transition-colors': true,
-            })}
-            href={nav.href}
-          >
-            {nav.label}
+    <nav className="flex mb-5 h-14 items-center border-b">
+      <Container>
+        <Flex gap="5" align="center">
+          <Link href="/">
+            <AiFillBug />
           </Link>
-        ))}
-      </ul>
+          <ul className="flex gap-3">
+            {navbars?.map((nav) => (
+              <Link
+                key={nav.label}
+                className={classNames({
+                  'text-zinc-900': nav.href === currentPath,
+                  'text-zinc-500': currentPath !== nav.href,
+                  'hover:text-zinc-800 transition-colors': true,
+                })}
+                href={nav.href}
+              >
+                {nav.label}
+              </Link>
+            ))}
+          </ul>
+        </Flex>
+      </Container>
     </nav>
   );
 }
