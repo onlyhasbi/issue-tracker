336e141cf4dc9cef987b4a2aa91c6d61345cf2d9
Muhammad Hasbi <onlyhasbi@gmail.com>
2023-12-08 00:39:57 +0800

    implement sortable issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -22,14 +22,22 @@ const columns: { label: string; value: keyof Issue; className?: string }[] = [
 
 async function IssuePage({ searchParams }: Props) {
   const statuses = Object.values(Status);
+
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const orderBy =
+    searchParams.orderBy &&
+    columns.map((column) => column.value).includes(searchParams.orderBy)
+      ? { [searchParams.orderBy]: 'asc' }
+      : undefined;
 
   const issues = await prisma.issue.findMany({
     where: {
       status,
     },
+    orderBy,
   });
 
   return (
