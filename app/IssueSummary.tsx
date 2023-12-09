1c81f3576b6b0728703c9b00f98d19818b29099b
Muhammad Hasbi <onlyhasbi@gmail.com>
2023-12-10 02:22:20 +0800

    build issues summary card

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,39 @@
+import { Status } from '@prisma/client';
+import { Card, Flex, Text } from '@radix-ui/themes';
+import Link from 'next/link';
+import React from 'react';
+
+type Props = {
+  open: number;
+  inProgress: number;
+  closed: number;
+};
+
+function IssueSummary({ open, inProgress, closed }: Props) {
+  const containers: { label: string; value: number; status: Status }[] = [
+    { label: 'Open Issues', value: open, status: 'OPEN' },
+    { label: 'In Progress Issues', value: inProgress, status: 'IN_PROGRESS' },
+    { label: 'Closed Issues', value: closed, status: 'CLOSED' },
+  ];
+  return (
+    <Flex gap="4">
+      {containers.map((container) => (
+        <Card key={container.status}>
+          <Flex direction="column" gap="4">
+            <Link
+              className="text-sm font-medium"
+              href={`/issues/list/?status=${container.status}`}
+            >
+              {container.label}
+            </Link>
+            <Text className="font-bold" size="7">
+              {container.value}
+            </Text>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  );
+}
+
+export default IssueSummary;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
+import { Box, Flex } from '@radix-ui/themes';
 import LatestIssue from './LatestIssue';
+import IssueSummary from './IssueSummary';
+import prisma from '@/prisma/client';
 
-export default function Home() {
-  return <LatestIssue />;
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: 'OPEN' } });
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+  const closed = await prisma.issue.count({ where: { status: 'CLOSED' } });
+
+  return (
+    <Flex direction="column" gap="5">
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+      <LatestIssue />
+    </Flex>
+  );
 }
