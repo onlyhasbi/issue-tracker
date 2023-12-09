5419573c01b5e648296c0d54580dd44e60ee3ab6
Muhammad Hasbi <onlyhasbi@gmail.com>
2023-12-10 02:50:22 +0800

    customize chart

--- app/IssueChart.tsx
+++ app/IssueChart.tsx
@@ -1,7 +1,14 @@
 'use client';
 
 import { Card } from '@radix-ui/themes';
-import { ResponsiveContainer, BarChart, XAxis, YAxis, Bar } from 'recharts';
+import {
+  ResponsiveContainer,
+  CartesianGrid,
+  BarChart,
+  XAxis,
+  YAxis,
+  Bar,
+} from 'recharts';
 import React from 'react';
 
 type Props = { open: number; inProgress: number; closed: number };
@@ -13,15 +20,23 @@ function IssueChart({ open, inProgress, closed }: Props) {
     { label: 'Closed', value: closed },
   ];
   return (
-    <Card>
+    <Card className="py-5 pr-1">
       <ResponsiveContainer width="100%" height={300}>
-        <BarChart data={data}>
-          <XAxis dataKey="label" />
-          <YAxis />
+        <BarChart
+          data={data}
+          margin={{
+            right: 15,
+            left: -15,
+            top: 10,
+          }}
+        >
+          <CartesianGrid vertical={false} strokeDasharray="3 3" />
+          <XAxis dataKey="label" fontSize={12} strokeOpacity={0} />
+          <YAxis fontSize={12} strokeOpacity={0} />
           <Bar
             dataKey="value"
-            barSize={60}
-            style={{ fill: 'var(--accent-9)' }}
+            barSize={75}
+            style={{ fill: 'var(--purple-9)' }}
           />
         </BarChart>
       </ResponsiveContainer>
