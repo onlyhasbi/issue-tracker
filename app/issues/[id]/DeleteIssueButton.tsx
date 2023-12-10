bbc0f107f96fc3c52b9e785f49722fbf36367899
Muhammad Hasbi <onlyhasbi@gmail.com>
2023-12-10 20:29:58 +0800

    fix delete button issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -28,7 +28,7 @@ function DeleteIssueButton({ issueId }: { issueId: number }) {
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button asChild={true} color="red" disabled={isDelete}>
+          <Button color="red" variant="surface" disabled={isDelete}>
             <AiOutlineForm />
             {isDelete && <Spinner />}
             Delete Issue
@@ -41,7 +41,7 @@ function DeleteIssueButton({ issueId }: { issueId: number }) {
           </AlertDialog.Description>
           <Flex mt="3" gap="3" justify="end">
             <AlertDialog.Action>
-              <Button asChild={true} color="red" onClick={handleDeleteIssue}>
+              <Button color="red" variant="surface" onClick={handleDeleteIssue}>
                 Delete Issue
               </Button>
             </AlertDialog.Action>
